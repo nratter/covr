76c793bed78fa53936d1a149ee7ab19e6221c6ee
Nick Rattermann <nrattermann@gmail.com>
2013-02-13 09:15:18 -0500

    initial commit2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Covr::Application.config.secret_token = '0981adb8afb12d64a4accb32fe0b99268c55e92d15b3c0a828abf6c90185b194918e2ac53e04dc4cceb0c41e69a08e323bc7b007ecbf215fada2ecc674b91a33'
